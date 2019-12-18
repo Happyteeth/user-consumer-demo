d20249d2179669aca32d993af3acf87e5f694511
Happyteeth <1043828048@qq.com>
2019-12-18 10:05:54 +0800

    test

--- user-consumer-demo/src/main/java/com/example/demo/service/UserService.java
+++ user-consumer-demo/src/main/java/com/example/demo/service/UserService.java
@@ -2,8 +2,10 @@ package com.example.demo.service;
 
 import com.example.demo.dao.UserDao;
 import com.example.demo.entity.User;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,11 +14,18 @@ import java.util.List;
 public class UserService {
 @Autowired
 private UserDao userDao;
+    @Autowired
+    private RestTemplate restTemplate;
 
+
+public List<User> errorQueryByIds(List<Long> ids){
+    return null;
+}
+    @HystrixCommand(fallbackMethod = "errorQueryByIds")
 public List<User> queryUserByIds(List<Long> ids){
     List<User> users = new ArrayList<>();
     for (Long id : ids) {
-        User user = this.userDao.queryUserById(id);
+        User user =this.userDao.queryUserById(id);
         users.add(user);
     }
     return users;
