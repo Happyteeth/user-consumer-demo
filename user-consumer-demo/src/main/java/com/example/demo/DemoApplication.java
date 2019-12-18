e4eede830a13bed46db72e7a0c1f9864cfbcf77e
Happyteeth <1043828048@qq.com>
2019-12-18 10:11:26 +0800

    test

--- user-consumer-demo/src/main/java/com/example/demo/DemoApplication.java
+++ user-consumer-demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,19 +1,25 @@
 package com.example.demo;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.client.OkHttp3ClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @EnableEurekaClient
+@EnableCircuitBreaker
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @EnableFeignClients
+@EnableHystrixDashboard
 public class DemoApplication {
 
 	public static void main(String[] args) {
@@ -25,5 +31,15 @@ public class DemoApplication {
 		// 这次我们使用了OkHttp客户端,只需要注入工厂即可
 		return new RestTemplate(new OkHttp3ClientHttpRequestFactory());
 
+	}
+
+	@Bean
+	public ServletRegistrationBean getServlet() {
+		HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+		ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+		registrationBean.setLoadOnStartup(1);
+		registrationBean.addUrlMappings("/hystrix.stream");
+		registrationBean.setName("HystrixMetricsStreamServlet");
+		return registrationBean;
 	}
 }
--- user-consumer-demo/src/main/java/com/example/demo/dao/UserDao.java
+++ user-consumer-demo/src/main/java/com/example/demo/dao/UserDao.java
@@ -1,6 +1,7 @@
 package com.example.demo.dao;
 
 import com.example.demo.entity.User;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
@@ -15,6 +16,7 @@ public class UserDao {
 private RestTemplate restTemplate;
 @Autowired
     DiscoveryClient discoveryClient;
+    @HystrixCommand(fallbackMethod = "fallback")
 public User queryUserById(Long id){
     // 在没使用ribbon时的写法，application类中没有调用@loadbalanced
 //    List<ServiceInstance> list = discoveryClient.getInstances("user-service");
@@ -23,4 +25,7 @@ public User queryUserById(Long id){
 //    return this.restTemplate.getForObject(url, User.class);
     return this.restTemplate.getForObject("http://user-service/user/"+1,User.class);
 }
+    public User fallback(Long id) {
+        return null;
+    }
 }
